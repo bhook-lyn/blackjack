ce1db5a55bfa683dacec9e15793cee93b3f68ead
hollyle1912 <83679505+hollyle1912@users.noreply.github.com>
2021-07-28 11:11:48 +1200

    Update components.py

--- components.py
+++ components.py
@@ -1,40 +1,66 @@
 import random 
 
-class Card(object):
+class Card:
     def __init__(self, value, suit):
         self.suit = suit
         self.value = value
 
     def __str__(self):
-        return 
-class Deck(object):
-    def __init__(self):
-        self.deck = []
-        build_deck() 
+        return " of ".join(self.value, self.suit)
 
-    def build_deck(self):
+class Deck:
+    def __init__(self):
         suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
-        values = [2, 3, 4, 5, 6, 7, 8, 9, "Jack", "Queen", "King", "Ace"]
+        values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+        """we'll take the face value of the card and then assigned blackjack 
+        point for cards later in each hand"""
+        self.all_cards = []
         for s in suits:
             for v in values:
-                self.deck.append(Card(s,v))
-
-    def show(self):
+                self.all_cards.append(Card(s,v))
 
     def shuffle(self):
-        self.deck
+         if len(self.all_cards) > 1:
+             random.shuffle(self.all_cards)
+        """randomize using shuffle func from the rand library"""
 
-class Hand(object):
-    def 
-"""class CompHand(object):
-    def __init__(self):
-        self.point = 0
-        self.hand 
+    def deal(self):
+        if len(self.all_cards) > 1: # as long as there're still cards left
+            dealt = self.all_cards.pop(0) # pop cards on top/left the deck
+            return dealt 
         
-    def hit(self):
+class Hand:
+    def __init__(self, dealer = False):
+        self.dealer = dealer
+        self.cards = []
+        self.score = 0 
 
-    def score()
-
-class PlayerHand(object):"""
+    def add_cards(self,card):
+        self.cards.append(card)
     
+    def calculate_score(self):
+        has_ace = False 
+        self.score = 0 
+        for card in self.cards:
+            if card.value.isnumeric() == True:
+                self.score += int(card.value)
+            elif card.value == "A":
+                has_ace = True 
+                self.score += 11
+            else:
+                self.score += 10 
+        if has_ace == True and self.score > 21:
+            self.score -= 10
+
+    def get_score(self):
+        self.calculate_score()
+        return self.score
+
+    def display(self):
+        if self.dealer:
+            (self.cards[1]).status
+            
         
+
+
+    
